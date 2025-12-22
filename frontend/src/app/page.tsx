cee9b8854347edf2bc42c540fd3dbc672e5ee4bf
Cameron <cameronlillis52@gmail.com>
2025-12-22 13:39:57 -0800

    Added hero left/right

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,8 @@ import axios from "axios";
 import { useState } from "react";
 import Header from "./components/ui/header/Header";
 import Faq from "./components/ui/faq-card/Faq";
-import Heroleft from "./components/ui/hero-left/Heroleft";
-import Heroright from "./components/ui/hero-right/Heroright";
+import HeroLeft from "./components/ui/hero-left/HeroLeft";
+import HeroRight from "./components/ui/hero-right/HeroRight";
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -29,8 +29,8 @@ export default function Home() {
         id="landing-page"
         className="min-h-screen bg-[radial-gradient(1000px_circle_at_center,#121639_0%,#000000_100%,#1C1F45_100%)] bg-fixed bg-no-repeat bg-center grid items-center gap-16 grid-cols-[1fr_1.2fr]"
       >
-        <Heroleft />
-        <Heroright />
+        <HeroLeft />
+        <HeroRight />
       </section>
       <section
         id="faq"
